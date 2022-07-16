81ec85dcb2eb8854fb38fc59783fb16127f72c46
David <c8c96387@gmail.com>
2022-07-16 16:00:29 +0800

    Complete the project

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-/* randomly return rock, paper, scissors*/
-function computerPlay() {
-    let choice  = ["rock", "paper", "scissors"];
-    return choice = choice[Math.floor(Math.random()*choice.length)];
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTIONS = [
+  {
+    name: 'rock',
+    emoji: '✊',
+    beats: 'scissors'
+  },
+  {
+    name: 'paper',
+    emoji: '✋',
+    beats: 'rock'
+  },
+  {
+    name: 'scissors',
+    emoji: '✌',
+    beats: 'paper'
+  }
+]
+
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener('click', e => {
+    const selectionName = selectionButton.dataset.selection
+    const selection = SELECTIONS.find(selection => selection.name === selectionName)
+    makeSelection(selection)
+  })
+})
+
+function makeSelection(selection) {
+  const computerSelection = randomSelection()
+  const yourWinner = isWinner(selection, computerSelection)
+  const computerWinner = isWinner(computerSelection, selection)
+
+  addSelectionResult(computerSelection, computerWinner)
+  addSelectionResult(selection, yourWinner)
+
+  if (yourWinner) incrementScore(yourScoreSpan)
+  if (computerWinner) incrementScore(computerScoreSpan)
 }
 
-/* Compare two results and who is winner*/
-function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt("you");
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerPlay();   
-    let result;
-
-    if (playerSelection == computerSelection) {
-        return result = "Tie!";
-    } else if ((playerSelection == "rock" && computerSelection == "scissors")
-    || (playerSelection == "scissors" && computerPlay == "paper") || (playerSelection == "paper" && computerSelection == "rock")) {
-        return result = "You win!";
-    } else {
-        return result = "You lose!";
-    }
+function incrementScore(scoreSpan) {
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
 }
 
-function game() {
-    for (let i = 0; i < 5; i++){
-        console.log(playRound());
-    }
+function addSelectionResult(selection, winner) {
+  const div = document.createElement('div')
+  div.innerText = selection.emoji
+  div.classList.add('result-selection')
+  if (winner) div.classList.add('winner')
+  finalColumn.after(div)
 }
 
-nextPrime
-for(i = 2 ;i < n;i++){
-    for(j = 2; j < i; j++){
-        if(i % j == 0) continue nextPrime;
-    }
-    alert(i);
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+  return SELECTIONS[randomIndex]
 }
